e55a562e21b9774bb07af58ab33ca705a2432c56
Wouter van Harten <wouter@woutervanharten.nl>
2018-09-24 16:24:43 +0200

    update to update script

--- update.sh
+++ update.sh
@@ -4,4 +4,5 @@ if [ -f "/data/RadixEnschedeBot/update_tally" ]; then
     cd /data/RadixEnschedeBot
     git pull| tee /home/wouter/tally.log
     rm /data/RadixEnschedeBot/update_tally
+    python3 /data/RadixEnschedeBot/RadixEnschedeBot.py restart
 fi
